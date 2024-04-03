83b286334452fec4f89b447b318df60a82036157
Getzels De Los Santos <get.delossantos@gmail.com>
2024-04-02 21:30:58 -0400

    Render dynamic content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./component/header/Header";
 import CoreConcept from "./component/coreConcept/CoreConcept";
+import TabButton from "./component/tabButton/TabButton";
+import {CORE_CONCEPTS, EXAMPLES} from "./data/data-with-examples";
 
 function App() {
+    const [selectedTab, setSelectedTab] = useState('components');
+
+    function handleSelect(selectedButton) {
+        console.log(selectedButton);
+        setSelectedTab(selectedButton);
+    }
+
     return (
         <div>
             <Header></Header>
@@ -14,7 +23,24 @@ function App() {
                         <CoreConcept />
                     </ul>
                 </section>
-                <section></section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                    </menu>
+                    <div id="tab-content">
+                        <h1>{EXAMPLES[selectedTab].title}</h1>
+                        <p>{EXAMPLES[selectedTab].description}</p>
+                        <pre>
+                            <code>
+                                {EXAMPLES[selectedTab].code}
+                            </code>
+                        </pre>
+                    </div>
+                </section>
             </main>
         </div>
   );
