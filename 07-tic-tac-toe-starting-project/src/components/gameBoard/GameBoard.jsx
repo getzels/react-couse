37a38ba5219b5615f181099f2a4057b162cd9e41
Getzels De Los Santos <get.delossantos@gmail.com>
2024-04-19 00:25:07 -0400

    80. Best Practice: Updating Object State Immutably

--- 07-tic-tac-toe-starting-project/src/components/gameBoard/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/gameBoard/GameBoard.jsx
@@ -1,16 +1,27 @@
+import {useState} from "react";
+
 export function GameBoard() {
     const initialGameBoard = Array.from({ length: 3 }, () => Array(3).fill(null));
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleGameBoardUpdate = (rowIndex, colIndex) => {
+        setGameBoard(board => {
+            const arrayCopy = [...board.map(innerArray => [...innerArray])];
+            arrayCopy[rowIndex][colIndex] = "X";
+            return arrayCopy;
+        });
+    }
 
     return (
         <ol id="game-board">
             {
-                initialGameBoard.map((row, index) =>
-                    <li key={row + index}>
+                gameBoard.map((row, rowIndex) =>
+                    <li key={row + rowIndex}>
                         <ol>
                             {
-                                row.map((column, index) =>
-                                    <li key={column + index}>
-                                        <button>{column}</button> {/*Player symbol*/}
+                                row.map((column, colIndex) =>
+                                    <li key={column + colIndex}>
+                                        <button onClick={() => handleGameBoardUpdate(rowIndex, colIndex)}>{column}</button> {/*Player symbol*/}
                                     </li>)
                             }
                         </ol>
